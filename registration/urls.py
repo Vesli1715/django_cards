fcaf30eb82cff54fcb1304abd2df2318cd40e6a9
bilobran <bethebest1715@gmail.com>
2019-02-14 12:20:32 +0200

    created user registration with email confirmation letter

--- registration/urls.py
+++ registration/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from . import views
 
 
 urlpatterns = [
     path('content/', views.index, name='index'),
-    path('singup/', views.singup, name='singup'),
+    path('signup/', views.test_signup, name='signup'),
     path('secret/', views.secret_page, name='secret'),
     path('secret_class/', views.SecretPage.as_view(), name='secret_class'),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    # path('test_home', views.test_home, name='test_home'),
+    path('test_signup/', views.test_signup, name='test_signup'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+         views.activate, name='activate'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -11,7 +11,7 @@ def index(request):
     return render(request, 'registration/content.html', {'count': count })
 
 
-def singup(request):
+def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -19,7 +19,7 @@ def singup(request):
             return redirect('content.html')
     else:
         form = UserCreationForm()
-    return render(request, 'registration/singup.html', {'form':form})
+    return render(request, 'registration/signup.html', {'form':form})
 
 
 @login_required
@@ -29,3 +29,59 @@ def secret_page(request):
 
 class SecretPage(LoginRequiredMixin, TemplateView):
     template_name = 'registration/secret_page.html'
+
+
+
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignupForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
+def test_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('registration/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
